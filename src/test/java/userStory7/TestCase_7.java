7a577953127702731d5dff5d41e78ef7b7658343
awettedla <awettedla@gmail.com>
2021-02-05 17:14:27 -0500

    intial commit

---
+++ src/test/java/userStory7/TestCase_7.java
@@ -0,0 +1,88 @@
+package userStory7;
+
+import com.trycloud.utilities.BrowserUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+public class TestCase_7 {
+
+
+    WebDriver driver;
+
+    @Test
+    public void test1() {
+
+            WebDriver driver = WebDriverFactory.getDriver("chrome");
+            driver.get("http://qa.trycloud.net/index.php/login?clear=1");
+            driver.manage().window().maximize();
+
+        WebElement userName= driver.findElement(By.xpath("//input[@id='user']"));
+        userName.click();
+        userName.sendKeys("User21");
+
+        BrowserUtils.sleep(1);
+
+        WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+        password.click();
+        password.sendKeys("Userpass123");
+
+        BrowserUtils.sleep(1);
+
+        WebElement loginButton= driver.findElement(By.xpath("//input[@id='submit-form']"));
+        loginButton.click();
+
+        WebElement Searchbar = driver.findElement(By.xpath("//div[@class='header-menu unified-search']"));
+        Searchbar.click();
+
+        String expectedresult=Searchbar.getAttribute("Search apps,files,comments,desk");
+        String actuallresult=Searchbar.getText();
+
+        Assert.assertEquals(expectedresult,actuallresult);
+
+        /*driver.findElement(By.id("user")).sendKeys("User21");
+        WebElement user = driver.findElement(By.xpath("//xfh"));
+        driver.findElement(By.id("password")).sendKeys("Userpass123");
+        driver.findElement(By.id("submit-wrapper")).click();
+        driver.findElement(By.className("material-design-icon__svg")).click();
+*/
+
+
+
+
+    }
+
+
+
+
+
+
+    }
+
+/*
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        driver.manage().window().maximize();
+
+    List<WebElement> listOflinks = driver.findElements(By.xpath("//body//a"));
+
+        for(WebElement eachLink : listOflinks){
+
+        System.out.println(eachLink.getText());
+    }
+
+    int numberoflinks=listOflinks.size();
+
+        System.out.println("number of links" + numberoflinks);
+
+        driver.close();
+*/
+
+
+
+
+
+
+
--- src/test/java/utilities/BrowserUtils.java
+++ src/test/java/utilities/BrowserUtils.java
@@ -1,4 +1,64 @@
 package utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
 public class BrowserUtils {
+    // Create method name:  wait
+    // Converting milliseconds to second
+    // handle checked exception
+
+
+    /**
+     * Pause test for some time
+     *
+     * @param second
+     */
+    public static void sleep(int second) {
+        second *= 1000l;
+
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("Something is wrong in the sleep method.");
+        }
+    }
+
+
+    //PRACTICE #4: Method: verifyOrder
+    //• Create a method named verifyOrder in WebOrderUtils class.
+    //• Method takes WebDriver object and String(name).
+    //• Method should verify if given name exists in orders.
+    //• This method should simply accepts a name(String), and assert whether
+    //given name is in the list or not.
+    //• Create a new TestNG test to test if the method is working as expected.
+
+    public static void verifyOrder(WebDriver driver, String expectedName){
+
+        //1- Create a locator that is returning us all of the lists in the table
+        //table[@id='ctl00_MainContent_orderGrid']//tr//td[2]
+
+        List<WebElement> allNames = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr//td[2]"));
+
+        //we need to loop through 'allName' List of WebElement and make sure 'expectedName' is in there
+
+        for (WebElement each : allNames) {
+
+            if (each.getText().equals(expectedName)) {
+                Assert.assertTrue(each.getText().equals(expectedName) );
+                return;
+            }
+
+        }
+
+        //The only condition where the Assert.fail() line below executed is if 'expectedName' is not in the list.
+
+        //Assert.fail(); method will FAIL THE TEST NO MATTER WHAT.
+        Assert.fail("The expectedName is not in the table. ");
+
+}
 }
